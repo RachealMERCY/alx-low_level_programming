0044a9bb3d841e4a6a78bcd1661790e8888d7539
RachealMERCY <mercyrachy@gmail.com>
2023-05-04 08:46:25 -0700

    2. strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,7 +13,8 @@ char *_strncpy(char *dest, char *src, int n)
 {
 int count_of_bytes;
 
-for (count_of_bytes = 0; count_of_bytes < n && src[count_of_bytes] != '\0'; count_of_bytes++)
+for (count_of_bytes = 0; count_of_bytes < n && src[count_of_bytes] != '\0';
+count_of_bytes++)
 dest[count_of_bytes] = src[count_of_bytes];
 
 for (; count_of_bytes < n; count_of_bytes++)
