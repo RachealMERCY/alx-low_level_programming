a12897ba3bc56e87f9302f287c6b807920daf792
RachealMERCY <mercyrachy@gmail.com>
2023-05-04 09:40:53 -0700

    5. Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * string_toupper - function that chnages all lowercase letters of
+ * a string to uppercase.
+ * @x: pointer to string.
+ *
+ * Return: pointer to uppercase string.
+ */
+
+char *string_toupper(char *x)
+{
+int length_of_string;
+
+length_of_string = 0;
+
+while (x[length_of_string] != '\0')
+{
+if (x[length_of_string] >= 97 && x[length_of_string] <= 122)
+{
+x[length_of_string] = x[length_of_string] - 32;
+}
+length_of_string++;
+}
+return (x);
+}
