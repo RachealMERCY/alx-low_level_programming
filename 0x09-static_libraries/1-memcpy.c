fdca2915f9d85158e791cbdf1871d0bfb05133a6
RachealMERCY <mercyrachy@gmail.com>
2023-05-16 06:12:42 -0700

    0. A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -4,7 +4,7 @@
  * _memcpy - a function that copies memory area
  * @dest: memory where is stored
  * @src: memory where is copied
- * n: number of bytes
+ * @n: number of bytes
  *
  * Return: copied memory with n byted changed
  */
