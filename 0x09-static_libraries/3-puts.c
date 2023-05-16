443acb27cb988b191a54499f48f517012410ae6c
RachealMERCY <mercyrachy@gmail.com>
2023-05-16 05:53:05 -0700

    A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -9,9 +9,6 @@ void _puts(char *s);
 
 int main(void)
 {
-	
-	puts("\"Programming is like building a multilingual puzzle");
+	_puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
-
