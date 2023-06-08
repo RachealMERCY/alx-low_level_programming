151cc8f449bfee784d3b1a4ee5b684502af8f035
RachealMERCY <mercyrachy@gmail.com>
2023-06-08 10:47:33 -0700

    Write a function that converts a binary number to an unsigned int.

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,44 +1,31 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * _stoi - converts chars to ints
- * @c: char to convert
- * Return: converted int
- */
-unsigned int _stoi(char c)
-{
-return ((unsigned int) c - '0');
-}
-/**
- * _strlen - calculates the length of the string
- * @s: input
- * Return: length of string
- */
-unsigned int _strlen(const char *s)
-{
-unsigned int i;
-for (i = 0; s[i]; i++)
-;
-return (i);
-}
-/**
- * binary_to_uint - converts a string of 1's and 0's to a decimal number
- * @b: string to convert
- * Return: unsigned decimal number
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
+ *
+ * Return: the converted number
  */
 unsigned int binary_to_uint(const char *b)
 {
-int i;
-unsigned int result, tmp, expo;
-if (!b)
+unsigned int total, power;
+int len;
+
+if (b == NULL)
 return (0);
-result = tmp = 0;
-expo = 1;
-for (i = _strlen(b) - 1; b[i]; i--, expo *= 2)
+
+for (len = 0; b[len]; len++)
 {
-if (b[i] != '0' && b[i] != '1')
+if (b[len] != '0' && b[len] != '1')
 return (0);
-tmp = _stoi(b[i]);
-result += tmp *expo;
 }
-return (result);
+
+for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
+{
+if (b[len] == '1')
+total += power;
+}
+
+return (total);
 }
