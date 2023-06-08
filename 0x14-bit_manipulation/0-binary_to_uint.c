599bcf8de721023092fee41299f2658c42d2195d
RachealMERCY <mercyrachy@gmail.com>
2023-06-08 10:36:42 -0700

    Write a function that converts a binary number to an unsigned int.

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,24 +1,44 @@
 #include "main.h"
-
 /**
- * binary_to_uint - converts a binary number to unsigned int
- * @b: string containing the binary number
- *
- * Return: the converted number
+ * _stoi - converts chars to ints
+ * @c: char to convert
+ * Return: converted int
+ */
+unsigned int _stoi(char c)
+{
+return ((unsigned int) c - '0');
+}
+/**
+ * _strlen - calculates the length of the string
+ * @s: input
+ * Return: length of string
+ */
+unsigned int _strlen(const char *s)
+{
+unsigned int i;
+for (i = 0; s[i]; i++)
+;
+return (i);
+}
+/**
+ * binary_to_uint - converts a string of 1's and 0's to a decimal number
+ * @b: string to convert
+ * Return: unsigned decimal number
  */
 unsigned int binary_to_uint(const char *b)
 {
 int i;
-unsigned int dec_val = 0;
-
+unsigned int result, tmp, expo;
 if (!b)
 return (0);
-
-for (i = 0; b[i]; i++)
+result = tmp = 0;
+expo = 1;
+for (i = _strlen(b) - 1; b[i]; i--, expo *= 2)
 {
-if (b[i] < '0' || b[i] > '1')
+if (b[i] != '0' && b[i] != '1')
 return (0);
-dec_val = 2 * dec_val + (b[i] - '0');
+tmp = _stoi(b[i]);
+result += tmp *expo;
 }
-return (dec_val);
+return (result);
 }
